11c0ad8dc41e14921c6d2df338e0831ccf6f1c55
VishwakarmaSaurabh <321saurabhv@gmail.com>
2020-06-03 11:05:45 +0530

    Updates

---
+++ precache-manifest.8983805d66c83e28d251f8036bba63f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77acdc97fdd05f7c8fa128e6442cc45d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f494a7e2144b9c6ca2c1",
+    "url": "/monsters-rolodex/static/css/main.207eebfc.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f494a7e2144b9c6ca2c1",
+    "url": "/monsters-rolodex/static/js/main.f5b7933d.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
